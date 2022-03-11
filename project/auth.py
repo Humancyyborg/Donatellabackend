2ecf70576682e8b03c94f707b4a621a0c9eae618
Adaobi <Adaobi@minerva.kgi.edu>
2022-03-11 18:55:17 +0000

    Including comments in the files

--- project/auth.py
+++ project/auth.py
@@ -1,14 +1,22 @@
+# This file contains the preliminary authentication routes defined for the app
+
+# Import the necessary modules
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
 
+# Initialize auth
 auth = Blueprint('auth', __name__)
 
+#creating the routes for the different portions of the page
+
+# Login route
 @auth.route('/login')
 def login():
     return render_template('login.html')
 
+# signup route
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
@@ -16,31 +24,32 @@ def signup():
 @auth.route('/signup', methods=['POST'])
 def signup_post():
 
-    # code to validate and add user to database goes here
+    # validate and add user to database
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    # Tackle reduandancy with account creation
+    user = User.query.filter_by(email=email).first() # checks if a user already exists, if user is returned, the the email is already contained in the database. 
 
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
+    if user: # if user already exists, redirect back to signup page.
 
-        #however, if user already exists, we need to first flag it before redirecting to the login page
+        #Alerts the user that the email already exists
         flash('Email address already exists')
         return redirect(url_for('auth.signup'))
 
-    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    # create a new user with the form data. Also, Hash the password so the plaintext version isn't saved.
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
 
-    # add the new user to the database
+    # add new user to the database
     db.session.add(new_user)
     db.session.commit()
 
 
 
-    # code to validate and add user to database goes here
+    # validate and add user to database goes here
     return redirect(url_for('auth.login'))
-
+# Logout for the route
 @auth.route('/logout')
 def logout():
     return render_template('signup.html')
--- project/main.py
+++ project/main.py
@@ -1,12 +1,18 @@
+# This file contains the routes for the non-auth portions of the app
+
+
+# Import necessary modules
 from flask import Blueprint, render_template
 from . import db
 
+# Initialize main
 main = Blueprint('main', __name__)
 
+# index route
 @main.route('/')
 def index():
     return render_template('index.html')
-
+# route for profile
 @main.route('/profile')
 def profile():
     return render_template('profile.html')
